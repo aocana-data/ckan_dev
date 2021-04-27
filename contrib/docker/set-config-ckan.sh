3978459709a73b46eb6c4569a0f39a2a8d578c11
20375558646 <gastonjouglard.10@gmail.com>
2021-04-27 20:54:47 -0300

    Add optional Harvest config

--- contrib/docker/set-config-ckan.sh
+++ contrib/docker/set-config-ckan.sh
@@ -11,5 +11,12 @@ sudo docker exec ckan ckan config-tool /etc/ckan/production.ini "ckan.plugins = 
 # De necesitarse los plugins seriestiempoexplorer & harvest
 #sudo docker exec ckan ckan config-tool /etc/ckan/production.ini "ckan.plugins = stats text_view image_view recline_view datastore xloader hierarchy_display hierarchy_form gobar_theme seriestiempoarexplorer harvest ckan_harvester"
 
-## Configuración en el config-file de CKAN para el Xloader
+## Configuracion del config-file de CKAN para el Xloader
 sudo docker exec ckan ckan config-tool /etc/ckan/production.ini "ckanext.xloader.jobs_db.uri = postgresql://ckan:ckan@db/ckan"
+
+##Settings Harvest
+#sudo docker exec ckan ckan config-tool /etc/ckan/production.ini "ckan.harvest.mq.type = redis"
+#sudo docker exec ckan ckan config-tool /etc/ckan/production.ini "ckan.harvest.mq.hostname = localhost"
+#sudo docker exec ckan ckan config-tool /etc/ckan/production.ini "ckan.harvest.mq.port = 6379"
+#sudo docker exec ckan ckan config-tool /etc/ckan/production.ini "ckan.harvest.mq.redis_db = 0"
+#sudo docker exec ckan ckan config-tool /etc/ckan/production.ini "ckan.harvest.mq.password =" 
